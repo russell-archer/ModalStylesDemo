2b500726ce9ab432ec9dd61625974f7794f51905
russell-archer <russell.archer@mac.com>
2019-09-30 23:12:30 +0100

    Initial Commit

---
+++ ModalStylesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModalStylesDemo
+//
+//  Created by Russell Archer on 30/09/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
